17cd1dd7a4a4c629dfaa7b9162177c3d0f3c272a
coracaos <eduardo-mce@hotmail.com>
2023-08-19 11:28:32 -0500

    uibuild

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('post-body', (req, res) => {
   }
 })
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] - :post-body'))
